02af981ec5735083dcf781f3ce081811f15e0a7f
lansuri <lansurisuresh1612@gmail.com>
2025-10-31 15:13:38 +0530

    commit

--- react-main-pjct/vite.config.js
+++ react-main-pjct/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:"0.0.0.0",
-    allowedHost:['localhost',         
-      '127.0.0.1',               
-      'react-e37l.onrender.com']
-  }
 })
+  
